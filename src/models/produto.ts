8b27b04abbced8b838e0c881ecd4243c7c2b462e
lourivaldantas <lourival.dneto2005@gmail.com>
2025-12-03 15:07:07 -0300

    ajustes

--- src/models/produto.ts
+++ src/models/produto.ts
@@ -1,25 +1,28 @@
 export class Produto {
+    constructor(
+        private _codigo: number,
+        private _valor: number,
+        private _descricao: string
+    ) {}
+    
     public get codigo(): number {
-        return this._codigo;
+    return this._codigo;
     }
     public set codigo(value: number) {
         this._codigo = value;
     }
+
     public get valor(): number {
         return this._valor;
     }
     public set valor(value: number) {
         this._valor = value;
     }
+    
     public get descricao(): string {
         return this._descricao;
     }
     public set descricao(value: string) {
         this._descricao = value;
     }
-    constructor(
-        private _codigo: number,
-        private _valor: number,
-        private _descricao: string
-    ) {}
 }
